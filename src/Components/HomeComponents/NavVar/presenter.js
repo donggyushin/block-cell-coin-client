00b4e3a84a6be9e0d9459bab549b8a1e096d8fff
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-02-13 21:42:15 +0900

    asdasd

--- src/Components/HomeComponents/NavVar/presenter.js
+++ src/Components/HomeComponents/NavVar/presenter.js
@@ -6,23 +6,25 @@ const cx = classNames.bind(styles);
 
 const NavVarPresenter = () => {
   return (
-    <div className={cx("container")}>
-      <div className={cx("logo")}>
-        <img
-          alt={"logo"}
-          src={require("../../../Media/Images/Home/NavVar/logo.png")}
-        />
-      </div>
-      <div className={cx("items")}>
-        <span>ABOUT US</span>
-        <span>PLATFORM</span>
-        <span>ALLOCATIONS</span>
-        <span>FUTURED</span>
-        <span>ROAD MAP</span>
-        <span>OUR TEAM</span>
-        <span>
-          WHITE PAPER <i class="fas fa-chevron-down" />
-        </span>
+    <div className={cx("outer")}>
+      <div className={cx("container")}>
+        <div className={cx("logo")}>
+          <img
+            alt={"logo"}
+            src={require("../../../Media/Images/Home/NavVar/logo.png")}
+          />
+        </div>
+        <div className={cx("items")}>
+          <span>ABOUT US</span>
+          <span>PLATFORM</span>
+          <span>ALLOCATIONS</span>
+          <span>FUTURED</span>
+          <span>ROAD MAP</span>
+          <span>OUR TEAM</span>
+          <span>
+            WHITE PAPER <i class="fas fa-chevron-down" />
+          </span>
+        </div>
       </div>
     </div>
   );
